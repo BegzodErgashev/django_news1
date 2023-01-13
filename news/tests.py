4fffcf3a3e6df32f41306885d6642364af4f2f0a
Begzod Ergashev <106820455+BegzodErgashev@users.noreply.github.com>
2023-01-13 18:17:11 +0500

    first commit

---
+++ news/tests.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import Post
+
+
+# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title='Theme', text='text of news')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_title = f'{post.title}'
+        expected_object_text = f'{post.text}'
+        self.assertEqual(expected_object_title, 'Theme')
+        self.assertEqual(expected_object_text, 'text of news')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title='Theme 2', text='text of other news')
+
+    def test_views_url_exists_at_proper_location(self):
+        resp = self.client.get("/")
+        self.assertEqual(resp.status_code, 200)
+
+    def test_views_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_views_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
